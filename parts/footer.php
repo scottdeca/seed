7b9bb88c501d6c4200150d40cb8d729b8bb8330b
Scott Deca <decasc@gmail.com>
2014-06-02 16:36:44 -0400

    Removed old mobile footer

--- parts/footer.php
+++ parts/footer.php
@@ -1,20 +1,4 @@
 <footer>
-	<form>
-		<div class="row collapse">
-			<p><b>Stay connected.</b><br />Sign up for our newsletter, <b><i>Dripfeed</i></b>™</p>
-			<div class="small-7 columns">
-				<input type="email" placeholder="Your email address" name="email" />
-			</div>
-			<div class="small-5 columns">
-				<a href="#" class="postfix radius">Sign me up</a>
-			</div>
-		</div>
-	</form>
-	
-	<p class="copyright">Copyright ® Seed Coworking LLC. <br class="show-for-small-only" /><a href="#">Privacy & Terms.</a></p>
-</footer>
-
-<footer>
 	
 	<div class="row">
 		<p class="left"><b>Stay connected.</b><br class="show-for-small-only" /> Sign up for our newsletter, <b><i>Dripfeed</i></b>™</p>
