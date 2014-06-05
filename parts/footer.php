cf9dae0add923c83547306c64ca9a08664db3394
Scott Deca <decasc@gmail.com>
2014-06-05 11:45:33 -0400

    footer refinements

--- parts/footer.php
+++ parts/footer.php
@@ -1,8 +1,9 @@
 <footer>
 	
 	<div class="row">
-		<p class="left medium-7 columns"><b>Stay connected.</b><br class="show-for-small-only" /> Sign up for our newsletter, <b><i>Dripfeed</i></b>™</p>
-		<form class="inline-form medium-5 columns">
+	
+		<p class="left medium-12 large-7 columns"><b>Stay connected.</b><br class="show-for-small-only" /> Sign up for our newsletter, <b><i>Dripfeed</i></b>™</p>
+		<form class="inline-form medium-12 large-5 columns">
 			<div class="row collapse inset-button">
 				<div class="small-12 columns">
 					<input type="email" placeholder="Your email address" name="email" />
@@ -15,6 +16,8 @@
 	
 	</div>
 	
-	<p class="copyright row">Copyright ® Seed Coworking LLC. <br class="show-for-small-only" /><a href="#">Privacy & Terms.</a></p>
+	<div class="row">
+		<p class="copyright small-12 columns">Copyright ® Seed Coworking LLC. <br class="show-for-small-only" /><a href="#">Privacy & Terms.</a></p>
+	</div>
 	
 </footer>
